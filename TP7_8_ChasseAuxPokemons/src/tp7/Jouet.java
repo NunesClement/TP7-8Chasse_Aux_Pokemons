3f840d41fb84e43fca345892b3b798cdceea204b
Minemega19 <themega19@gmail.com>
2018-04-03 16:35:58 +0200

    Add files via upload

--- TP7_8_ChasseAuxPokemons/src/tp7/Jouet.java
+++ TP7_8_ChasseAuxPokemons/src/tp7/Jouet.java
@@ -1,5 +1,14 @@
 package tp7;
 
 public class Jouet extends Item implements Utilisable, Modifiable{
-
+	private int apportAppetit;
+	private int apportSatisfaction;
+	private int apportLoyaute;
+	
+	public Jouet(String nom, int frequence, int apportAppetit, int apportSatisfaction, int apportLoyaute) {
+		super(nom, frequence);
+		this.apportAppetit = apportAppetit;
+		this.apportSatisfaction = apportSatisfaction;
+		this.apportLoyaute = apportLoyaute;
+	}
 }
